ccb2dddb49940632c0eda43588ef51a1357fe57c
ajikia15 <aleksandrej15@gmail.com>
2025-10-23 18:06:41 +0400

    feat: Add action buttons for "Shop Now" and "Contact Us" in HeroMarqueeSection for enhanced user interaction

--- src/pages/homepage/hero/HeroMarqueeSection.tsx
+++ src/pages/homepage/hero/HeroMarqueeSection.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import SimpleMarquee from "@/components/fancy/blocks/simple-marquee";
 import { FeatureCard, featuresData } from "./FeatureCard";
+import { ArrowRight, Send } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const MarqueeItem = ({ children }: { children: React.ReactNode }) => (
   <div className="mb-2 cursor-pointer overflow-hidden rounded duration-300 ease-in-out hover:scale-105">
@@ -81,6 +83,23 @@ export default function HeroMarqueeSection() {
             </MarqueeItem>
           ))}
         </SimpleMarquee>
+      </div>
+      <div className="absolute bottom-0 left-0 right-0 flex items-center justify-evenly gap-2 border-t border-neutral-600 bg-neutral-800 px-5 py-3">
+        <div className="flex flex-1 items-center justify-between rounded-full bg-neutral-900 p-1 pl-4 font-semibold">
+          SHOP NOW
+          <Button variant="secondary" className="h-8 w-8 rounded-full">
+            <ArrowRight className="" />
+          </Button>
+        </div>
+        <div className="flex flex-1 items-center justify-between rounded-full border border-neutral-700 p-1 pl-4 font-semibold">
+          CONTACT US
+          <Button
+            variant="ghost"
+            className="h-8 w-8 rounded-full bg-neutral-900"
+          >
+            <Send />
+          </Button>
+        </div>
       </div>
     </div>
   );
