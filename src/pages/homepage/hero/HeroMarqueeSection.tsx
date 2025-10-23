2a65ae0d68a4949ba3189540853f7d7b1a983fe7
ajikia15 <aleksandrej15@gmail.com>
2025-10-23 18:46:51 +0400

    feat: Enhance HeroMarqueeSection with responsive layout and multiple marquee directions for improved user experience

--- src/pages/homepage/hero/HeroMarqueeSection.tsx
+++ src/pages/homepage/hero/HeroMarqueeSection.tsx
@@ -35,54 +35,115 @@ export default function HeroMarqueeSection() {
       className="relative h-full w-full overflow-hidden rounded-xl bg-neutral-800"
       // style={{ perspective: "1200px" }}
     >
-      <div
-        className="absolute inset-0 mr-5 flex origin-center flex-row items-center justify-end space-x-4 p-5 will-change-transform"
-        style={{
-          // transform: "rotateX(-20deg) rotateY(-30deg)",
-          transformStyle: "preserve-3d",
-        }}
-      >
-        <SimpleMarquee
-          className="h-full"
-          baseVelocity={10}
-          repeat={4}
-          easing={easeFn}
-          direction="up"
-        >
-          {firstThird.map((feature, i) => (
-            <MarqueeItem key={i}>
-              <FeatureCard feature={feature} />
-            </MarqueeItem>
-          ))}
-        </SimpleMarquee>
+      {/* Desktop Layout - Backdrop Logo/Text with Absolute Positioned Marquees */}
+      <div className="hidden h-full sm:block">
+        {/* Backdrop Logo - Left Center */}
+        <div className="absolute left-0 top-1/2 z-0 flex -translate-y-1/2 items-center justify-start p-8">
+          <img
+            src="/logo-white.svg"
+            alt="Kaido Logo"
+            className="h-64 w-auto opacity-5 md:h-80 lg:h-96"
+          />
+        </div>
 
-        <SimpleMarquee
-          className="h-full"
-          baseVelocity={10}
-          repeat={4}
-          easing={easeFn}
-          direction="down"
-        >
-          {secondThird.map((feature, i) => (
-            <MarqueeItem key={i}>
-              <FeatureCard feature={feature} />
-            </MarqueeItem>
-          ))}
-        </SimpleMarquee>
+        {/* Right Side - Absolute Positioned Vertical Marquees */}
+        <div className="absolute inset-0 mr-5 flex origin-center flex-row items-center justify-end space-x-3 p-4 will-change-transform md:space-x-4 md:p-5">
+          <SimpleMarquee
+            className="h-full"
+            baseVelocity={10}
+            repeat={4}
+            easing={easeFn}
+            direction="up"
+          >
+            {firstThird.map((feature, i) => (
+              <MarqueeItem key={i}>
+                <FeatureCard feature={feature} />
+              </MarqueeItem>
+            ))}
+          </SimpleMarquee>
 
-        <SimpleMarquee
-          className="h-full"
-          baseVelocity={10}
-          repeat={4}
-          easing={easeFn}
-          direction="up"
-        >
-          {lastThird.map((feature, i) => (
-            <MarqueeItem key={i}>
-              <FeatureCard feature={feature} />
-            </MarqueeItem>
-          ))}
-        </SimpleMarquee>
+          <SimpleMarquee
+            className="h-full"
+            baseVelocity={10}
+            repeat={4}
+            easing={easeFn}
+            direction="down"
+          >
+            {secondThird.map((feature, i) => (
+              <MarqueeItem key={i}>
+                <FeatureCard feature={feature} />
+              </MarqueeItem>
+            ))}
+          </SimpleMarquee>
+
+          <SimpleMarquee
+            className="h-full"
+            baseVelocity={10}
+            repeat={4}
+            easing={easeFn}
+            direction="up"
+          >
+            {lastThird.map((feature, i) => (
+              <MarqueeItem key={i}>
+                <FeatureCard feature={feature} />
+              </MarqueeItem>
+            ))}
+          </SimpleMarquee>
+        </div>
+      </div>
+
+      {/* Mobile Layout - Horizontal Marquees */}
+      <div className="flex h-full flex-col sm:hidden">
+        {/* Mobile Header with Logo */}
+        <div className="flex items-center justify-between p-4">
+          <img src="/logo-white.svg" alt="Kaido Logo" className="h-6 w-auto" />
+          <h2 className="text-lg font-bold text-white">Premium Laptops</h2>
+        </div>
+
+        {/* Mobile Horizontal Marquees */}
+        <div className="flex flex-1 flex-col space-y-4 p-4">
+          <SimpleMarquee
+            className="h-32"
+            baseVelocity={15}
+            repeat={3}
+            easing={easeFn}
+            direction="left"
+          >
+            {firstThird.map((feature, i) => (
+              <MarqueeItem key={i}>
+                <FeatureCard feature={feature} />
+              </MarqueeItem>
+            ))}
+          </SimpleMarquee>
+
+          <SimpleMarquee
+            className="h-32"
+            baseVelocity={15}
+            repeat={3}
+            easing={easeFn}
+            direction="right"
+          >
+            {secondThird.map((feature, i) => (
+              <MarqueeItem key={i}>
+                <FeatureCard feature={feature} />
+              </MarqueeItem>
+            ))}
+          </SimpleMarquee>
+
+          <SimpleMarquee
+            className="h-32"
+            baseVelocity={15}
+            repeat={3}
+            easing={easeFn}
+            direction="left"
+          >
+            {lastThird.map((feature, i) => (
+              <MarqueeItem key={i}>
+                <FeatureCard feature={feature} />
+              </MarqueeItem>
+            ))}
+          </SimpleMarquee>
+        </div>
       </div>
       <div className="absolute bottom-0 left-0 right-0 flex items-center justify-evenly gap-2 border-t border-neutral-600 bg-neutral-800 px-5 py-3">
         <div className="flex flex-1 items-center justify-between rounded-full bg-neutral-900 p-1 pl-4 font-semibold">
