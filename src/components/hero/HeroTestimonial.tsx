73c64f93f518bf5218bee45dca669835fac6f55a
ajikia15 <aleksandrej15@gmail.com>
2025-10-29 19:58:46 +0400

    refactor: Simplify HeroTestimonial layout by removing backdrop logo and repositioning HelperSearchBar

--- src/components/hero/HeroTestimonial.tsx
+++ src/components/hero/HeroTestimonial.tsx
@@ -2,18 +2,9 @@ import HelperSearchBar from "./HelperSearchBar";
 export default function HeroTestimonial() {
   return (
     <div className="relative flex h-full w-full overflow-hidden rounded-xl bg-neutral-800">
-      {/* Desktop Layout - Backdrop Logo/Text with Absolute Positioned Marquees */}
-      <div className="hidden h-full sm:block">
-        {/* Backdrop Logo - Left Center */}
-        <div className="absolute left-0 top-1/2 z-0 flex -translate-y-1/2 items-center justify-start p-8">
-          <img
-            src="/logo-white.svg"
-            alt="Kaido Logo"
-            className="h-64 w-auto opacity-5 md:h-80 lg:h-96"
-          />
-        </div>
+      <div className="absolute bottom-0 left-0 right-0 z-10 px-4 pb-4">
+        <HelperSearchBar />
       </div>
-      <HelperSearchBar />
       {/* <div className="absolute bottom-0 left-0 right-0 flex items-center justify-evenly gap-2 border-t border-neutral-600 bg-neutral-800 px-5 py-3">
         <div className="flex flex-1 items-center justify-between rounded-full bg-neutral-900 p-1 pl-4 font-semibold">
           SHOP NOW
